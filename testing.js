1df49d92e2c414374d7d9ff920cd1a33b3478fe5
AmixemHello <99071709+AmixemHello@users.noreply.github.com>
2022-12-25 01:52:04 +0100

    Add files via upload

---
+++ testing.js
@@ -0,0 +1,26 @@
+class StrictEqualityExtension {
+  getInfo() {
+    return {
+      id: 'minecrafttools', // change this if you make an actual extension!
+      name: 'Minecraft Tools',
+      blocks: [
+        {
+          opcode: 'setinangle',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'set your minecraft character to [ANGLE]',
+          arguments: {
+            ANGLE: {
+              type: Scratch.ArgumentType.ANGLE,
+              defaultValue: '90'
+            }
+          }
+        }
+      ]
+    };
+  }
+  setinangle() {
+    // Note strict equality: Inputs must match exactly: in type, case, etc.
+    return "Error";
+  }
+}
+Scratch.extensions.register(new StrictEqualityExtension());
